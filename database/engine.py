2e7861107ee7a44546689e88524a444a3ab5f10b
islam <dujsobaevislam01@gmail.com>
2024-10-15 20:27:37 +0600

    deploy rayway

--- database/engine.py
+++ database/engine.py
@@ -3,8 +3,8 @@ from sqlalchemy.ext.asyncio import AsyncSession, async_sessionmaker, create_asyn
 
 from .model import Base
 
-# engine = create_async_engine(os.getenv('DB_LITE'), echo=True)
-engine = create_async_engine(os.getenv('DB_URL'), echo=True)
+engine = create_async_engine(os.getenv('DB_LITE'), echo=True)
+# engine = create_async_engine(os.getenv('DB_URL'), echo=True)
 session_maker = async_sessionmaker(bind=engine, class_=AsyncSession, expire_on_commit=False)
 
 
